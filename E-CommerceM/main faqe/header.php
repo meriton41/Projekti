e51c433a1c742082cef9593cef359c82055caf20
meriton <137052060+meriton41@users.noreply.github.com>
2024-02-01 00:45:50 +0100

    aa

--- E-CommerceM/main faqe/header.php
+++ E-CommerceM/main faqe/header.php
@@ -20,14 +20,11 @@
         <ul class="Librat">
             <a href="e-commerce.php">Phones</a>
             <a href="Computer.php">Computers&Servers</a>
-            <a href="#">Tablets</a>
-<<<<<<< HEAD
+            <a href="Tablet.php">Tablets</a>
             <a href="Main.php">Home</a>
-=======
->>>>>>> 17e6ff26b820404b89e309f0854ab5e098e82702
-            <a href="us.php">About<gap>Us</a>
-            <a href="ContactUs.php">Contact<gap>Us</a>
-            <a href="LogOut.php" style='color:blue;'>Log<gap>Out</a>
+            <a href="us.php">About<Us</a>
+            <a href="ContactUs.php">ContactUs</a>
+            <a href="LogOut.php" style='color:blue;'>LogOut</a>
         </ul>
     </nav>
     <style>
